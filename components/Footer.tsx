c0d4481a5ecc20299df3a34f2292821692cd5ab3
Mark Alber <marcalber59@gmail.com>
2025-08-08 01:16:52 +0300

    added whatsapp icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import styles from '@/app/Footer.module.css';
+import { FaWhatsapp } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
@@ -22,6 +23,14 @@ const Footer: React.FC = () => {
           &copy; {currentYear} SoftSpark. All rights reserved.
         </div>
       </div>
+      <a
+        href="https://wa.me/+201224213669"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed z-50 bottom-6 right-6 bg-green-500 rounded-full p-3 shadow-lg hover:bg-green-600 transition-colors"
+      >
+        <FaWhatsapp className="text-white text-3xl" />
+      </a>
     </footer>
   );
 };
