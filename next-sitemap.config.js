71abc4d0f0babc8afa5ef983a52b1a7a543f7050
Mark Alber <marcalber59@gmail.com>
2025-08-27 18:02:13 +0300

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://softspark.me',
+  generateRobotsTxt: true, // optional
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
